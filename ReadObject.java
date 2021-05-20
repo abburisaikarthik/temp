eb4e803b02d1d09cb408be29f0e3055175f3d80b
Abburi Sai Karthik <abburisaikarthik@gmail.com>
2021-05-20 15:40:30 +0530

    Modified

--- ReadObject.java
+++ ReadObject.java
@@ -4,15 +4,7 @@ public class ReadObject {
 	public static void main(String[] args) {
 		File f = new File("test2.dat");
 		try 
-		{
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream(f));
-			
-			Student s = (Student)in.readObject();//Object class object
-			
-			System.out.println(s.n);
-			System.out.print(s.x);
-		} 
-		catch (IOException | ClassNotFoundException e) 
+		e) 
 		{
 			
 			e.printStackTrace();
